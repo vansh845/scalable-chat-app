72f499eac970de9a9b27c586b39e970420ae5fb9
vansh845 <103567533+vansh845@users.noreply.github.com>
2024-02-07 09:42:37 +0530

    backend almost done

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,37 @@
-import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+import { createServer } from 'http';
+import { Redis } from 'ioredis';
+const httpServer = createServer();
+const pub = new Redis();
+const sub = new Redis();
+const PORT = process.env.PORT ? process.env.PORT : 8000
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+})
+httpServer.listen(process.env.PORT ?? 8000)
+sub.subscribe('message_queue')
 
-const server = createServer((req, res) => {
-    res.end('Hello World')
+sub.on('message', (channel, message) => {
+    if (channel === 'message_queue') {
+        io.emit('message', message)
+    }
+    console.log(message)
 })
 
-server.listen(3001)
+
+io.on('connect', (socket) => {
+    console.log('user connected');
+    socket.on('greet', (greet) => {
+        console.log(greet)
+    });
+    socket.on('message', (message) => {
+        console.log(message)
+        const result = pub.publish('message_queue', message);
+    });
+
+})
--- apps/server/ḍist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_http_1 = require("node:http");
-const server = (0, node_http_1.createServer)((req, res) => {
-    res.end('Hello World');
-});
-server.listen(3001);
